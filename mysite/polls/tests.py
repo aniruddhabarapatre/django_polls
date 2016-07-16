7f0e6aba188648911a48efd51a9a10734c35d006
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-07-15 22:32:55 -0400

    Tests for questions published date

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,26 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+class QuestionMethodTests(TestCase):
+
+  def test_was_published_recently_with_future_question(self):
+    # should return false for question whose pub_date is in future
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question = Question(pub_date=time)
+    self.assertEqual(future_question.was_published_recently(), False)
+
+  def test_was_published_recently_with_old_question(self):
+    # should return false for question whose pub_date is older than 1 day
+    time = timezone.now() - datetime.timedelta(days=30)
+    old_question = Question(pub_date=time)
+    self.assertEqual(old_question.was_published_recently(), False)
+
+  def test_was_published_recently_with_recent_question(self):
+    # should return true for question whose pub_date is within last day
+    time = timezone.now() - datetime.timedelta(hours=1)
+    recent_question = Question(pub_date=time)
+    self.assertEqual(recent_question.was_published_recently(), True)
