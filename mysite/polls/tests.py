ad43ad9bb0d22d08872f2611849d448661ce571d
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2016-07-16 13:20:43 -0400

    Adding tests for detail view

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -51,6 +51,21 @@ class QuestionViewTests(TestCase):
       ['<Question: Past question 2.>', '<Question: Past question 1.>']
     )
 
+class QuestionIndexDetailTests(TestCase):
+  def test_detail_view_with_future_question(self):
+    # detail view of question with future pub_date returns 404
+    future_question = create_question(question_text="Future question.", days=30)
+    url = reverse('polls:detail', args=(future_question.id,))
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
+
+  def test_detail_view_with_past_question(self):
+    # detail view of question with past pub_date should show question
+    past_question = create_question(question_text="Past question.", days=-30)
+    url = reverse('polls:detail', args=(past_question.id,))
+    response = self.client.get(url)
+    self.assertContains(response, past_question.question_text)
+
 class QuestionMethodTests(TestCase):
 
   def test_was_published_recently_with_future_question(self):
